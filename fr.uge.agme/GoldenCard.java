5bdc3be0e84d81cea999196b3c3add7a67f0d8ab
alexios30 <147252629+alexios30@users.noreply.github.com>
2024-04-01 11:44:08 +0200

    Update GoldenCard.java

--- fr.uge.agme/GoldenCard.java
+++ fr.uge.agme/GoldenCard.java
@@ -7,7 +7,7 @@ import java.nio.file.Path;
 import java.util.ArrayList;
 import java.util.Objects;
 
-public record GoldenCard(String cornerTopLeft, String cornerBottomLeft, String cornerTopRight, String cornerBottomRight,String kindgom, int cost, char typescoring,char scoring) {
+public record GoldenCard(String cornerTopLeft, String cornerBottomLeft, String cornerTopRight, String cornerBottomRight,String kingdom, int cost, char typescoring,char scoring) {
 	
 	public GoldenCard{
 		Objects.requireNonNull(cornerTopLeft);
@@ -33,11 +33,11 @@ public record GoldenCard(String cornerTopLeft, String cornerBottomLeft, String c
     public static GoldenCard addgoldencard(String line) {
         String[] parts = line.split(" ");
 
-        String cornerTopLeft = parts[3];
-        String cornerBottomLeft = parts[4];
-        String cornerTopRight = parts[5];
-        String cornerBottomRight = parts[6];
-        String kindgom =  parts[8];
+        String cornerTopLeft = parts[2];
+        String cornerBottomLeft = parts[3];
+        String cornerTopRight = parts[4];
+        String cornerBottomRight = parts[5];
+        String kindgom =  parts[7];
         String dernier = parts[parts.length - 1];
         
         var typescoring = dernier.charAt(0);
@@ -61,13 +61,40 @@ public record GoldenCard(String cornerTopLeft, String cornerBottomLeft, String c
         GoldenCard card = new GoldenCard(cornerTopLeft, cornerBottomLeft, cornerTopRight, cornerBottomRight, kindgom, animalCount, typescoring,scoring);
         return card;
     }
+    public static String getcornerTopLeft(GoldenCard card) {
+    	return card.cornerTopLeft;
+    }
+    
+    public static String getcornerBottomLeft(GoldenCard card) {
+    	return card.cornerBottomLeft;
+    }
+    
+    public static String getcornerTopRight(GoldenCard card) {
+    	return card.cornerTopRight;
+    }
+    public static String getcornerBottomRight(GoldenCard card) {
+    	return card.cornerBottomRight;
+    }
+    public static String getKingdom(GoldenCard card) {
+    	return card.kingdom;
+    }
+    public static int getCost(GoldenCard card) {
+    	return card.cost;
+    }
+    
+    public static char gettypescoring(GoldenCard card) {
+    	return card.typescoring;
+    }
 
-
+    public static char getscoring(GoldenCard card) {
+    	return card.scoring;
+    }
+    
 	@Override
 	public String toString() {
 		return "GoldenCard [cornerTopLeft=" + cornerTopLeft + ", cornerBottomLeft=" + cornerBottomLeft
 				+ ", cornerTopRight=" + cornerTopRight + ", cornerBottomRight=" + cornerBottomRight + ", kindgom="
-				+ kindgom + ", cost=" + cost + ", typescoring=" + typescoring + ", scoring=" + scoring + "]" + "\n";
+				+ kingdom + ", cost=" + cost + ", typescoring=" + typescoring + ", scoring=" + scoring + "]" + "\n";
 	}
     
     
