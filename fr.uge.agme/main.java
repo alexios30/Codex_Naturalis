9c3baca1eebf5a28d2635bc36cedd439530b8981
alexios30 <147252629+alexios30@users.noreply.github.com>
2024-04-01 10:50:41 +0200

    Add files via upload

--- fr.uge.agme/main.java
+++ fr.uge.agme/main.java
@@ -2,6 +2,7 @@ package fr.uge.game;
 
 import fr.umlv.zen5.Application;
 import java.awt.Color;
+import java.awt.geom.Rectangle2D;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -18,5 +19,22 @@ public class main {
         var srcGolden= Path.of("include/Golden.txt");
         var packGolden = GoldenCard.createRessourceCard(srcGolden);
         System.out.println(packGolden);
+        
+        RessourceCard test = new RessourceCard ("Animal", "Animal", "Empty", "Animal", "2", 0);
+        
+        Application.run(Color.BLUE, context -> {
+			var screenInfo = context.getScreenInfo();
+			var width = screenInfo.getWidth();
+			var height = screenInfo.getHeight();
+			System.out.println("size of the screen (" + width + " x " + height + ")");
+
+			context.renderFrame(graphics -> {
+				graphics.setColor(Color.ORANGE);
+				graphics.fill(new Rectangle2D.Float(400, 800, 350, 150));
+				
+			});
+			SimpleGameView.dessincard(context, test, 400, 800, 350, 150);
+        });
+        
     }
 }
