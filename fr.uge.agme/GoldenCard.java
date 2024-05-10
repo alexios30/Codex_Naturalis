2a54935954bbee378a11a8c96b36150f250d581b
Nathan-sbl <157363847+Nathan-sbl@users.noreply.github.com>
2024-05-11 00:04:16 +0200

    Update GoldenCard.java

--- fr.uge.agme/GoldenCard.java
+++ fr.uge.agme/GoldenCard.java
@@ -6,15 +6,18 @@ import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.Objects;
 
-public record GoldenCard(String cornerTopLeft, String cornerBottomLeft, String cornerTopRight, String cornerBottomRight,String kingdom, int cost, char typescoring,char scoring) implements Card {
+public record GoldenCard(String cornerTopLeft, String cornerBottomLeft, String cornerTopRight, String cornerBottomRight,String kingdom, HashMap<String, Integer> cost, char typescoring, char scoring) implements Card {
 	
 	public GoldenCard{
 		Objects.requireNonNull(cornerTopLeft);
         Objects.requireNonNull(cornerBottomLeft);
         Objects.requireNonNull(cornerTopRight);
         Objects.requireNonNull(cornerBottomRight);
+        Objects.requireNonNull(kingdom);
+        Objects.requireNonNull(cost);
 	}
 	
 	
@@ -23,7 +26,7 @@ public record GoldenCard(String cornerTopLeft, String cornerBottomLeft, String c
             String line;
             ArrayList<GoldenCard>golden= new ArrayList<>();
             while ((line = reader.readLine()) != null) {
-            	 var card =GoldenCard.addgoldencard(line);
+            	 var card = GoldenCard.addgoldencard(line);
             	 golden.add(card);
 
         } 
@@ -33,35 +36,58 @@ public record GoldenCard(String cornerTopLeft, String cornerBottomLeft, String c
     
     public static GoldenCard addgoldencard(String line) {
         String[] parts = line.split(" ");
-
-        String cornerTopLeft = parts[2];
-        String cornerBottomLeft = parts[3];
-        String cornerTopRight = parts[4];
-        String cornerBottomRight = parts[5];
+        //System.out.println(line);
+        //System.out.println(parts);
+        HashMap<String, Integer> cost = new HashMap<String, Integer>();
+        //plateau.put(new Pair(0, 0), piocheRessource(getPackRessource()));
+        cost.put("Animal", 0);
+        cost.put("Fungi", 0);
+        cost.put("Insect", 0);
+        cost.put("Plant", 0);
+        
+        String cornerTopLeft = readCorner(parts[2]);
+        String cornerBottomLeft = readCorner(parts[3]);
+        String cornerTopRight = readCorner(parts[4]);
+        String cornerBottomRight = readCorner(parts[5]);
         String kindgom =  parts[7];
         String dernier = parts[parts.length - 1];
-        
+           
         var typescoring = dernier.charAt(0);
-        
         var scoring = dernier.charAt(2);
         
-        boolean countAnimals = false; 
-        int animalCount = 0;
-
-        for (String part : parts) {
-            if (part.equalsIgnoreCase("Cost")) {
-                countAnimals = true; 
-            } else if (part.equalsIgnoreCase("Scoring")) {
-                break; 
-            } else if (countAnimals && part.equalsIgnoreCase("Animal")) {
-                animalCount++;
-            }
-            
-            
+        for (int i = 9; i < parts.length - 2; i++) {
+            String mot = parts[i];
+            cost.put(mot, cost.getOrDefault(mot, 0) + 1);
         }
-        GoldenCard card = new GoldenCard(cornerTopLeft, cornerBottomLeft, cornerTopRight, cornerBottomRight, kindgom, animalCount, typescoring,scoring);
+        System.out.println(cost);
+        
+//        boolean countCost = false; 
+//        int animalCount = 0;
+//
+//        for (String part : parts) {
+//            if (part.equalsIgnoreCase("Cost")) {
+//            	countCost = true; 
+//            } else if (part.equalsIgnoreCase("Scoring")) {
+//                break; 
+//            } else if (countCost && part.equalsIgnoreCase("Animal")) {
+//                animalCount++;
+//            }
+//  
+//        }
+        
+        GoldenCard card = new GoldenCard(cornerTopLeft, cornerBottomLeft, cornerTopRight, cornerBottomRight, kindgom, cost, typescoring,scoring);
         return card;
     }
+    
+    public static String readCorner(String corner) {
+    	char firstChar = corner.charAt(0);
+    	// si la valeur est un artefact ou une ressource
+    	if (firstChar == 'A' || firstChar == 'R') {
+			return corner.substring(2);
+		}
+		return corner;    
+    }
+    
     public String cornerTopLeft() {
     	return cornerTopLeft;
     }
@@ -79,7 +105,7 @@ public record GoldenCard(String cornerTopLeft, String cornerBottomLeft, String c
     public String getKingdom() {
     	return kingdom;
     }
-    public int getCost() {
+    public HashMap<String, Integer> getCost() {
     	return cost;
     }
     
