897457ecf5333120e800ddf5660fa757ea604c50
alexios30 <147252629+alexios30@users.noreply.github.com>
2024-06-02 14:33:14 +0200

    Update Pair.java

--- fr.uge.agme/Pair.java
+++ fr.uge.agme/Pair.java
@@ -1,5 +1,16 @@
 package fr.uge.game;
 
 public record Pair(int x, int y) {
+    public Pair(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
 
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
 }
