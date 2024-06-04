0338338c22ffafbf7a8aefc7a78f104bf4eaad75
alexios30 <147252629+alexios30@users.noreply.github.com>
2024-06-04 15:50:16 +0200

    Update StarterCard.java

--- fr.uge.agme/StarterCard.java
+++ fr.uge.agme/StarterCard.java
@@ -2,6 +2,7 @@ package fr.uge.game;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Objects;
 
@@ -164,5 +165,9 @@ public class StarterCard implements Card {
             ", versoResources=" + Arrays.toString(versoResources) +
             ", verso=" + verso +
             '}';
+    }
+    public HashMap<String, Integer> returncost(){
+		return null;
+    	
     }
 }
